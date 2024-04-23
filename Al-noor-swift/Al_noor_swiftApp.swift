eb6f55f4337cce33bccf053fd56fa923fc0984b8
mansmahamat <mansour.mahamat.salle@gmail.com>
2024-04-23 22:30:37 +0200

    Initial Commit

---
+++ Al-noor-swift/Al_noor_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Al_noor_swiftApp.swift
+//  Al-noor-swift
+//
+//  Created by Mansour Mahamat-salle on 23/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct Al_noor_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
